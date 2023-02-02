c17dc56fc4e52b29c7a012b381552147a6482494
Hyungjoon <fhrek60@gmail.com>
2023-02-03 01:39:54 +0900

    신규_백준_1300_K번째 수 - 이분탐색 - 일단 이분탐색인거 전혀 못잡았고.. 찾는과정 자체가 좀 이해가 안된다. 기본 문제 풀어볼 것

--- Boong/Python/230128_백준_1263_시간 관리/1.py
+++ Boong/Python/230128_백준_1263_시간 관리/1.py
@@ -8,13 +8,15 @@ startTime = 99999999
 
 workList.sort(key=lambda x:x[1])
 
+# i번째 마감시간 이전에 사용시간들을 모두 더해서 마감시간보다 크다면 -1 return 하고 프로그램 종료
+# 아니라면 현재 마감시간에서 사용시간을 빼서 최소값에 저장
 for i in range(len(workList)):
     time = workList[i][0]
     for j in range(i):
         time += workList[j][0]
     if workList[i][1] < time:
         print(-1)
-        break
+        exit()
     else:
         startTime = min(startTime, workList[i][1] - time)
 
