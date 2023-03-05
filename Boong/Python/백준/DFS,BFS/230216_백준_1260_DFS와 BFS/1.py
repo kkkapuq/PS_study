97d688621e477de784c7ea392f6728ea529af36a
조형준 <fhrek@naver.com>
2023-03-05 15:14:13 +0900

    이전 내용 백업

--- Boong/Python/백준/DFS,BFS/230216_백준_1260_DFS와 BFS/1.py
+++ Boong/Python/백준/DFS,BFS/230216_백준_1260_DFS와 BFS/1.py
@@ -1,45 +1,81 @@
-'''
-문제 : DFS와 BFS
-난이도 : 실버 2
-링크 : https://www.acmicpc.net/problem/1260
-'''
+# '''
+# 문제 : DFS와 BFS
+# 난이도 : 실버 2
+# 링크 : https://www.acmicpc.net/problem/1260
+# '''
+
+# from collections import deque
+
+# n, m, v = map(int, input().split())
+
+# graph = [[] for _ in range(n+1)]
+# visited = [False] * (n+1)
+
+# def dfs(v):
+#     visited[v] = True
+#     print(v, end=' ')
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs(i)
+            
+# def bfs(v):
+#     visited = [False] * (n+1)
+#     q = deque([v])
+#     # 방문처리
+#     visited[v] = True
+#     while q:
+#         temp = q.popleft()
+#         print(temp, end=' ')
+#         for i in graph[temp]:
+#             if not visited[i]:
+#                 q.append(i)
+#                 visited[i] = True
+        
+
+# for i in range(1, m+1):
+#     x, y = map(int, input().split())
+#     graph[x].append(y)
+#     graph[y].append(x)
+#     graph[x].sort()
+#     graph[y].sort()
+    
+# dfs(v)
+# print()
+# bfs(v)
+
 
 from collections import deque
 
 n, m, v = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
+visited =  [False] * (n+1)
 
-def dfs(v):
-    visited[v] = True
-    print(v, end=' ')
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i)
-            
-def bfs(v):
-    visited = [False] * (n+1)
-    q = deque([v])
-    # 방문처리
-    visited[v] = True
-    while q:
-        temp = q.popleft()
-        print(temp, end=' ')
-        for i in graph[temp]:
-            if not visited[i]:
-                q.append(i)
-                visited[i] = True
-        
-
-for i in range(1, m+1):
+for i in range(1, n+1):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
     graph[x].sort()
     graph[y].sort()
     
+def dfs(v):
+    visited[i] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+
+def bfs(v):
+    visited = [False] * (n+1)
+    visited[i] = True
+    q = deque([v])
+    while q:
+        x = q.popleft()
+        print(x, end=' ')
+        for i in graph[x]:
+            if not visited[i]:
+                q.append(i)
+    
 dfs(v)
 print()
 bfs(v)
-    
--- Boong/Python/프로그래머스/레벨1/230212_프로그래머스_155652_둘만의 암호/1.py
+++ Boong/Python/프로그래머스/레벨1/230212_프로그래머스_155652_둘만의 암호/1.py
@@ -1,33 +1,54 @@
 def solution(s, skip, index):
-    answer = []
+    # answer = []
     
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-               'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z']
-    skipList = []
-    for i in skip:
-        skipList.append(i)
+    # alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+    #            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    #            'w', 'x', 'y', 'z']
+    # skipList = []
+    # for i in skip:
+    #     skipList.append(i)
         
-    for i in range(len(s)):
-        # 현재 위치
-        cur = alphabet.index(s[i])
-        tempNum = cur + index
-        # index개만큼 뒤에 있는걸 뽑아줘야 되는데, 이 때 skipList에 있으면 걔네를 무시하고 다음껄 count 해줘야함
-        while True:
-            for j in range(1, index+1):
-                temp = cur + j
-                if temp > 25:
-                    temp %= 26
-                if alphabet[temp] in skipList:
-                    tempNum += 1
-            if tempNum > 25:
-                tempNum %= 26
-            if alphabet[tempNum] not in skipList:
-                break
+    # for i in range(len(s)):
+    #     # 현재 위치
+    #     cur = alphabet.index(s[i])
+    #     tempNum = cur + index
+    #     # index개만큼 뒤에 있는걸 뽑아줘야 되는데, 이 때 skipList에 있으면 걔네를 무시하고 다음껄 count 해줘야함
+    #     while True:
+    #         # 현재위치에서 j만큼 범위를 탐색, skipList에 있다면 tempNum을 더해줘서 그만큼 더 탐색하게 함
+    #         for j in range(1, index+1):
+    #             temp = cur + j
+    #             # 25보다 크다면 26의 나머지만큼의 alphabet[temp] 로 되도록.
+    #             temp %= 26
+    #             # 그렇게 해서 변환된 temp가 skipList에 있다면 
+    #             if alphabet[temp] in skipList:
+    #                 tempNum += 1
+    #         if tempNum > 25:
+    #             tempNum %= 26
+    #         if alphabet[tempNum] not in skipList:
+    #             break
             
-        answer.append(alphabet[tempNum])
-                    
+    #     answer.append(alphabet[tempNum])
+    # return ''.join(answer)
     
-    return ''.join(answer)
+    answer = ''
+    
+    skip_dict = dict()
+    for skip_case in skip:
+        skip_dict[ord(skip_case)] = True
+    
+    for letter in s:
+        letter_code = ord(letter)
+        convert_count = 0
+        while convert_count < index:
+            letter_code += 1
+            if letter_code > ord('z'):
+                letter_code = ord('a')
+            if skip_dict.get(letter_code):
+                continue
+            else:
+                 convert_count += 1
+        answer += chr(letter_code)
+        
+    return answer
 
 print(solution('asdf', 'bcdetu', 5))
